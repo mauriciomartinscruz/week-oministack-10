b9c53210081fdd5a4a0f3f51e86f57b7f5376dfa
Mauricio Martins <mauricio@economizebr.com>
2020-02-04 00:45:30 -0300

    layout web

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+const routes = require('./routes')
 
 const app = express();
 
@@ -8,5 +10,9 @@ app.get('/', (request, response) => {
         message: 'Hello World, man'
     });
 });
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -11,28 +11,39 @@ import './Sidebar.css'
 
 function App() {
 
+  const [github_username, setGithubUsername] = useState('')
+  const [techs, setTechs] = useState('')
+
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('poss', position);
-        const { latitude, longitude } = position.coords
-
-        setLatitude(latitude)
-        setLongitude(longitude)
-      },
-      (err) => {
-        console.log('deu erro', err)
-        setLatitude(-123546)
-        setLongitude(-462153)
-      },
-      {
-        timeout: 30000
-      }
-    )
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log('poss', position);
+          const { latitude, longitude } = position.coords
+  
+          setLatitude(latitude)
+          setLongitude(longitude)
+        },
+        (err) => {
+          console.log('deu erro', err)
+          setLatitude(-123546)
+          setLongitude(-462153)
+        },
+        {
+          timeout: 30000
+        }
+      )
+    }
   }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault()
+
+    
+  }
 
   return (
     <div id="app">
@@ -41,11 +52,17 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input type="text" name="github_username" id="github_username" required />
+            <input type="text" name="github_username" 
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+              id="github_username" required />
           </div>
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input type="text" name="techs" id="techs" required />
+            <input type="text" name="techs"
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            id="techs" required />
           </div>
 
           <div className="input-group">
